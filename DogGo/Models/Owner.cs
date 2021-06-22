a3016bb54ecae16f561a361fd96a1352329ba4b7
Carter <carter.culkin@gmail.com>
2021-06-22 16:02:00 -0500

    Only pushing up what's working at the moment

--- DogGo/Models/Owner.cs
+++ DogGo/Models/Owner.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DogGo.Models
 {
     public class Owner
@@ -8,6 +10,10 @@ namespace DogGo.Models
         public string Address { get; set; }
         public int NeighborhoodId { get; set; }
         public Neighborhood Neighborhood { get; set; }
+        [Required(ErrorMessage = "You must provide a phone number")]
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number, please format it like (123)-456-7890")]
         public string Phone { get; set; }
     }
 }
